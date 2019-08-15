65d2615455d54f08fafa84e755b18dc617786167
tessein@gmail.com <tessein@gmail.com>
2019-08-15 12:41:48 -0400

    switch to nanos

--- project-euler/src/Euler005.java
+++ project-euler/src/Euler005.java
@@ -12,7 +12,7 @@ public class Euler005 {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		final long startTime = System.currentTimeMillis();
+		final long startTime = System.nanoTime();
 		final int alimit = 20;
 		for (long i = alimit; i <= Long.MAX_VALUE; i += alimit) {
 			boolean done = true;
@@ -23,7 +23,7 @@ public class Euler005 {
 				}
 			if (done) {
 				System.out.println("Euler005 = " + i + " - runtime = "
-				    + (System.currentTimeMillis() - startTime) + " millis");
+				    + (System.nanoTime() - startTime) + " nanos");
 				break;
 			}
 		}
