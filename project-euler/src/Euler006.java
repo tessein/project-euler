11f0faf511fe94a11953cee633f5a3e86bd0e54d
tessein@gmail.com <tessein@gmail.com>
2019-08-19 07:55:40 -0400

    clean up

--- project-euler/src/Euler006.java
+++ project-euler/src/Euler006.java
@@ -1,28 +1,20 @@
 import java.util.stream.IntStream;
 
 public class Euler006 {
+	
+	
+	/**
+	 * main method to solve Euler 6
+	 * @param args
+	 */
 	public static void main(String[] args) {
-//		long startTime = System.nanoTime();
-//		int sumsquares = 0;
-//		int squaresum = 0;
-//		for (int i = 1; i <= 100; i++) {
-//			sumsquares += i * i;
-//			squaresum += i;
-//		}
-//		int answer = (squaresum * squaresum) - sumsquares;
-//		System.out.println("Euler006 = " + answer + " - finished in "
-//		    + (System.nanoTime() - startTime) + " nanos");
-//		startTime = System.nanoTime();
-//		System.out.println(sumOfSquaresIntStream() + " : " + (System.nanoTime() - startTime));
-//		
-//		startTime = System.nanoTime();
-//		System.out.println(squareSumIntStream() + " : " + (System.nanoTime() - startTime));
-		
+	
 		long startTime = System.nanoTime();
 		System.out.println( "Euler006 = " + 
 				(squareSumIntStream() - sumOfSquaresIntStream()) + " - finished in "
 			    + (System.nanoTime() - startTime) + " nanos");
 	}
+
 	
 	/**
 	 * Calculate sum of squares
@@ -32,6 +24,7 @@ public class Euler006 {
 		long sumOfSquares = IntStream.rangeClosed(1, 100).map(i -> i * i).sum();
 		return sumOfSquares;	
 	}
+
 	
 	/**
 	 * calculate square of xums
@@ -41,6 +34,5 @@ public class Euler006 {
 		long squaresum = IntStream.rangeClosed(1,  100).map(i -> i).sum();	
 		return squaresum * squaresum;	
 	}
-	
-	
+		
 }
